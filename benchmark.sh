8b106f5e1dd59c91163a9bb42d758c60ebcf7ad7
Iti Shree <itishree1999@gmail.com>
2023-04-28 14:54:46 +0100

    Perf command works in the shell now.

--- benchmark.sh
+++ benchmark.sh
@@ -37,49 +37,49 @@ else
     echo "No executable clang binary found at $CPATH"
 fi
 
+cd tmux
 
-if [ -d "profiles" ]; then
-    read -p "The 'profiles' directory already exists. Do you want to continue? (y/n) " answer
-    if [ "$answer" = "n" ]; then
-        echo "Directory profiles already exists, exiting script."
-        exit 1
-    fi
-else
-    mkdir profiles
-fi
+# Set the CFLAGS and DFLAGS variables
+CFLAGS="-std=gnu99 -O2 -g -Wno-long-long -Wall -W -Wformat=2
+-Wmissing-prototypes -Wstrict-prototypes -Wmissing-declarations -Wwrite-strings
+-Wshadow -Wpointer-arith -Wsign-compare -Wundef -Wbad-function-cast -Winline
+-Wcast-align -Wdeclaration-after-statement -Wno-pointer-sign -Wno-attributes
+-Wno-unused-result -Wno-format-y2k" DFLAGS="-DPACKAGE_NAME=\"tmux\"
+-DPACKAGE_TARNAME=\"tmux\" -DPACKAGE_VERSION=\"next-3.4\"
+-DPACKAGE_STRING=\"tmux next-3.4\" -DPACKAGE_BUGREPORT=\"\" -DPACKAGE_URL=\"\"
+-DPACKAGE=\"tmux\" -DVERSION=\"next-3.4\" -DSTDC_HEADERS=1 -DHAVE_SYS_TYPES_H=1
+-DHAVE_SYS_STAT_H=1 -DHAVE_STDLIB_H=1 -DHAVE_STRING_H=1 -DHAVE_MEMORY_H=1
+-DHAVE_STRINGS_H=1 -DHAVE_INTTYPES_H=1 -DHAVE_STDINT_H=1 -DHAVE_UNISTD_H=1
+-D__EXTENSIONS__=1 -D_ALL_SOURCE=1 -D_GNU_SOURCE=1 -D_POSIX_PTHREAD_SEMANTICS=1
+-D_TANDEM_SOURCE=1 -DHAVE_DIRENT_H=1 -DHAVE_FCNTL_H=1 -DHAVE_INTTYPES_H=1
+-DHAVE_PATHS_H=1 -DHAVE_PTY_H=1 -DHAVE_STDINT_H=1 -DHAVE_SYS_DIR_H=1
+-DHAVE_LIBM=1 -DHAVE_DIRFD=1 -DHAVE_FLOCK=1 -DHAVE_PRCTL=1 -DHAVE_SYSCONF=1
+-DHAVE_ASPRINTF=1 -DHAVE_CFMAKERAW=1 -DHAVE_CLOCK_GETTIME=1
+-DHAVE_EXPLICIT_BZERO=1 -DHAVE_GETDTABLESIZE=1 -DHAVE_GETLINE=1 -DHAVE_MEMMEM=1
+-DHAVE_SETENV=1 -DHAVE_STRCASESTR=1 -DHAVE_STRNDUP=1 -DHAVE_STRSEP=1
+-DHAVE_EVENT2_EVENT_H=1 -DHAVE_NCURSES_H=1 -DHAVE_B64_NTOP=1
+-DHAVE_MALLOC_TRIM=1 -DHAVE_DAEMON=1 -DHAVE_FORKPTY=1 -DHAVE___PROGNAME=1
+-DHAVE_PROGRAM_INVOCATION_SHORT_NAME=1 -DHAVE_PR_SET_NAME=1
+-DHAVE_SO_PEERCRED=1 -DHAVE_PROC_PID=1 -I. -D_DEFAULT_SOURCE
+-D_XOPEN_SOURCE=600 -DTMUX_VERSION=\"next-3.4\"
+-DTMUX_CONF=\"/etc/tmux.conf:~/.tmux.conf:$XDG_CONFIG_HOME/tmux/tmux.conf:~/.config/tmux/tmux.conf\"
+-DTMUX_TERM=\"tmux-256color\" -DDEBUG -iquote."
 
-ProfPath=$(pwd)/profles
+# Pass the CFLAGS and DFLAGS variables into the clang command
+perf record -o nolbr.data -- env CFLAGS="$CFLAGS" DFLAGS="$DFLAGS" clang -MT window-copy.o -MD -MP -MF $depbase.Tpo -c -o window-copy.o window-copy.c
 
-read -p "Collecting profile for No LBR clang build in directory $(pwd)/profiles. Continue (y/n)?"
+perf2bolt $CPATH/clang --nl -p $2 -o nolbr.fdata -w nolbr.yaml
+perf2bolt $CPATH/clang -p $3 -o lbr.fdata -w lbr.yaml
+perf2bolt $CPATH/clang -p $4 -o pt.fdata -w pt.yaml
 
-if [ "$answer" = "n" ]; then
-    echo "Exiting script."
-    exit 1
-fi
+llvm-bolt $CPATH/clang -o $CPATH/clang.nolbr -b nolbr.yaml \
+    -reorder-blocks=ext-tsp -reorder-functions=hfsort+ -split-functions \
+    -split-all-cold -dyno-stats -icf=1 -use-gnu-stack
 
-cd tmux && perf record -o $ProfPath/nolbr.data -- $CPATH/clang-16 -DPACKAGE_NAME=\"tmux\"
-    -DPACKAGE_TARNAME=\"tmux\" -DPACKAGE_VERSION=\"next-3.4\"
-    -DPACKAGE_STRING=\"tmux\ next-3.4\" -DPACKAGE_BUGREPORT=\"\" -DPACKAGE_URL=\"\"
-    -DPACKAGE=\"tmux\" -DVERSION=\"next-3.4\" -DSTDC_HEADERS=1 -DHAVE_SYS_TYPES_H=1
-    -DHAVE_SYS_STAT_H=1 -DHAVE_STDLIB_H=1 -DHAVE_STRING_H=1 -DHAVE_MEMORY_H=1
-    -DHAVE_STRINGS_H=1 -DHAVE_INTTYPES_H=1 -DHAVE_STDINT_H=1 -DHAVE_UNISTD_H=1
-    -D__EXTENSIONS__=1 -D_ALL_SOURCE=1 -D_GNU_SOURCE=1 -D_POSIX_PTHREAD_SEMANTICS=1
-    -D_TANDEM_SOURCE=1 -DHAVE_DIRENT_H=1 -DHAVE_FCNTL_H=1 -DHAVE_INTTYPES_H=1
-    -DHAVE_PATHS_H=1 -DHAVE_PTY_H=1 -DHAVE_STDINT_H=1 -DHAVE_SYS_DIR_H=1
-    -DHAVE_LIBM=1 -DHAVE_DIRFD=1 -DHAVE_FLOCK=1 -DHAVE_PRCTL=1 -DHAVE_SYSCONF=1
-    -DHAVE_ASPRINTF=1 -DHAVE_CFMAKERAW=1 -DHAVE_CLOCK_GETTIME=1
-    -DHAVE_EXPLICIT_BZERO=1 -DHAVE_GETDTABLESIZE=1 -DHAVE_GETLINE=1 -DHAVE_MEMMEM=1
-    -DHAVE_SETENV=1 -DHAVE_STRCASESTR=1 -DHAVE_STRNDUP=1 -DHAVE_STRSEP=1
-    -DHAVE_EVENT2_EVENT_H=1 -DHAVE_NCURSES_H=1 -DHAVE_B64_NTOP=1
-    -DHAVE_MALLOC_TRIM=1 -DHAVE_DAEMON=1 -DHAVE_FORKPTY=1 -DHAVE___PROGNAME=1
-    -DHAVE_PROGRAM_INVOCATION_SHORT_NAME=1 -DHAVE_PR_SET_NAME=1
-    -DHAVE_SO_PEERCRED=1 -DHAVE_PROC_PID=1 -I.  -D_DEFAULT_SOURCE
-    -D_XOPEN_SOURCE=600 -DTMUX_VERSION='"next-3.4"'
-    -DTMUX_CONF='"/etc/tmux.conf:~/.tmux.conf:$XDG_CONFIG_HOME/tmux/tmux.conf:~/.config/tmux/tmux.conf"'
-    -DTMUX_TERM='"tmux-256color"' -DDEBUG -iquote.       -std=gnu99 -O2 -g
-    -Wno-long-long -Wall -W -Wformat=2 -Wmissing-prototypes -Wstrict-prototypes
-    -Wmissing-declarations -Wwrite-strings -Wshadow -Wpointer-arith -Wsign-compare
-    -Wundef -Wbad-function-cast -Winline -Wcast-align -Wdeclaration-after-statement
-    -Wno-pointer-sign -Wno-attributes -Wno-unused-result -Wno-format-y2k    -MT
-    window-copy.o -MD -MP -MF $depbase.Tpo -c -o window-copy.o window-copy.c
+llvm-bolt $CPATH/clang -o $CPATH/clang.lbr -b lbr.yaml \
+    -reorder-blocks=ext-tsp -reorder-functions=hfsort+ -split-functions \
+    -split-all-cold -dyno-stats -icf=1 -use-gnu-stack
 
+llvm-bolt $CPATH/clang -o $CPATH/clang.pt -b pt.yaml \
+    -reorder-blocks=ext-tsp -reorder-functions=hfsort+ -split-functions \
+    -split-all-cold -dyno-stats -icf=1 -use-gnu-stack
