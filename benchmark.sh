dd4b71c5f51410b0dd97f2d69791b2c92092b991
iti shree <itishree1999@gmail.com>
2023-04-28 12:44:48 +0100

    Initial commit, added draft scripts.

---
+++ benchmark.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+set -e
+
+read -p "Current working directory is $(pwd). Want to change it? (y/n)" answer
+
+if [ "$answer" = "y" ]; then
+        exit
+fi
+
+if [ -d "tmux" ]; then
+    read -p "The 'tmux' directory already exists. Do you want to continue? (y/n) " answer
+    if [ "$answer" = "n" ]; then
+        echo "Directory tmux already exists, exiting script."
+        exit 1
+    fi
+else
+    git clone https://github.com/tmux/tmux.git
+
+    # Run autogen.sh and configure
+    cd tmux
+    sh autogen.sh
+    ./configure
+fi
+
+if [ $# -eq 0 ]; then
+    echo "Please provide the path to clang binary as an argument (e.g /usr/local/bin)"
+    exit 1
+fi
+
+CPATH=$1
+
+$CPATH/clang --version > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "Clang binary found at $CPATH"
+else
+    echo "No executable clang binary found at $CPATH"
+fi
+
+
+if [ -d "profiles" ]; then
+    read -p "The 'profiles' directory already exists. Do you want to continue? (y/n) " answer
+    if [ "$answer" = "n" ]; then
+        echo "Directory profiles already exists, exiting script."
+        exit 1
+    fi
+else
+    mkdir profiles
+fi
+
+ProfPath=$(pwd)/profles
+
+read -p "Collecting profile for No LBR clang build in directory $(pwd)/profiles. Continue (y/n)?"
+
+if [ "$answer" = "n" ]; then
+    echo "Exiting script."
+    exit 1
+fi
+
+cd tmux && perf record -o $ProfPath/nolbr.data -- $CPATH/clang-16 -DPACKAGE_NAME=\"tmux\"
+    -DPACKAGE_TARNAME=\"tmux\" -DPACKAGE_VERSION=\"next-3.4\"
+    -DPACKAGE_STRING=\"tmux\ next-3.4\" -DPACKAGE_BUGREPORT=\"\" -DPACKAGE_URL=\"\"
+    -DPACKAGE=\"tmux\" -DVERSION=\"next-3.4\" -DSTDC_HEADERS=1 -DHAVE_SYS_TYPES_H=1
+    -DHAVE_SYS_STAT_H=1 -DHAVE_STDLIB_H=1 -DHAVE_STRING_H=1 -DHAVE_MEMORY_H=1
+    -DHAVE_STRINGS_H=1 -DHAVE_INTTYPES_H=1 -DHAVE_STDINT_H=1 -DHAVE_UNISTD_H=1
+    -D__EXTENSIONS__=1 -D_ALL_SOURCE=1 -D_GNU_SOURCE=1 -D_POSIX_PTHREAD_SEMANTICS=1
+    -D_TANDEM_SOURCE=1 -DHAVE_DIRENT_H=1 -DHAVE_FCNTL_H=1 -DHAVE_INTTYPES_H=1
+    -DHAVE_PATHS_H=1 -DHAVE_PTY_H=1 -DHAVE_STDINT_H=1 -DHAVE_SYS_DIR_H=1
+    -DHAVE_LIBM=1 -DHAVE_DIRFD=1 -DHAVE_FLOCK=1 -DHAVE_PRCTL=1 -DHAVE_SYSCONF=1
+    -DHAVE_ASPRINTF=1 -DHAVE_CFMAKERAW=1 -DHAVE_CLOCK_GETTIME=1
+    -DHAVE_EXPLICIT_BZERO=1 -DHAVE_GETDTABLESIZE=1 -DHAVE_GETLINE=1 -DHAVE_MEMMEM=1
+    -DHAVE_SETENV=1 -DHAVE_STRCASESTR=1 -DHAVE_STRNDUP=1 -DHAVE_STRSEP=1
+    -DHAVE_EVENT2_EVENT_H=1 -DHAVE_NCURSES_H=1 -DHAVE_B64_NTOP=1
+    -DHAVE_MALLOC_TRIM=1 -DHAVE_DAEMON=1 -DHAVE_FORKPTY=1 -DHAVE___PROGNAME=1
+    -DHAVE_PROGRAM_INVOCATION_SHORT_NAME=1 -DHAVE_PR_SET_NAME=1
+    -DHAVE_SO_PEERCRED=1 -DHAVE_PROC_PID=1 -I.  -D_DEFAULT_SOURCE
+    -D_XOPEN_SOURCE=600 -DTMUX_VERSION='"next-3.4"'
+    -DTMUX_CONF='"/etc/tmux.conf:~/.tmux.conf:$XDG_CONFIG_HOME/tmux/tmux.conf:~/.config/tmux/tmux.conf"'
+    -DTMUX_TERM='"tmux-256color"' -DDEBUG -iquote.       -std=gnu99 -O2 -g
+    -Wno-long-long -Wall -W -Wformat=2 -Wmissing-prototypes -Wstrict-prototypes
+    -Wmissing-declarations -Wwrite-strings -Wshadow -Wpointer-arith -Wsign-compare
+    -Wundef -Wbad-function-cast -Winline -Wcast-align -Wdeclaration-after-statement
+    -Wno-pointer-sign -Wno-attributes -Wno-unused-result -Wno-format-y2k    -MT
+    window-copy.o -MD -MP -MF $depbase.Tpo -c -o window-copy.o window-copy.c
+
---
+++ build.sh
@@ -0,0 +1,184 @@
+#! /bin/sh
+
+set -e
+
+home=$HOME/report
+dirc=$1
+
+missing=0
+check_for () {
+    which $1 > /dev/null 2> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Error: can't find $1 binary"
+        missing=1
+    fi
+}
+
+if [ -z "$dirc" ]; then
+        dirc="bolt"
+fi
+
+TOPLEV=$home/$dirc
+
+if [ ! -d $TOPLEV ]; then
+        mkdir $TOPlEV
+fi
+
+cd $TOPLEV
+
+# Check if llvm-project repo exists, otherwise clone it
+if [ -d "llvm-project" ]; then
+        cd llvm-project
+        git pull
+        cd ..
+else
+        git clone https://github.com/nmdis1999/llvm-project.git
+fi
+
+if [ -d "build" ]; then
+    read -p "The 'build' directory already exists. Do you want to continue? (y/n) " answer
+    if [ "$answer" != "y" ]; then
+        echo "Exiting script."
+        exit 1
+    fi
+else
+    # Create a build directory and navigate to it
+    mkdir build
+    # Run cmake and ninja to build bolt
+    cmake -G Ninja ../llvm-project/llvm \
+        -DLLVM_TARGETS_TO_BUILD="X86;AArch64" -DCMAKE_BUILD_TYPE=Release \
+        -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="bolt"
+    ninja bolt install
+fi
+
+# Exporting path to llvm-bolt and perf2bolt binaries
+export PATH=$TOPLEV/build/bin:$PATH
+
+# Bootstrapping Clang v x.x.x with PGO and LTO
+
+echo "Enter name for stage1 clang build (default: stage1)"
+read stage1_dir
+
+if [ -z "$stage1_dir" ]; then
+    stage1_dir="stage1"
+fi
+
+# Build stage1 clang from default gcc
+
+if [ -d "${TOPLEV}/$stage1_dir" ]; then
+    read -p "The '${stage1_dir}' directory already exists. Do you want to continue? (y/n) " answer
+    if [ "$answer" != "y" ]; then
+        echo "Directory ${stage1_dir} already exists, exiting script."
+        exit 1
+    fi
+else
+    # Build stage1 clang from default gcc
+    mkdir "${TOPLEV}/$stage1_dir"
+    cd "${TOPLEV}/$stage1_dir"
+    cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD=X86 \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_ASM_COMPILER=gcc \
+        -DLLVM_ENABLE_PROJECTS="clang;lld" \
+        -DLLVM_ENABLE_RUNTIMES="compiler-rt" \
+        -DCOMPILER_RT_BUILD_SANITIZERS=OFF -DCOMPILER_RT_BUILD_XRAY=OFF \
+        -DCOMPILER_RT_BUILD_LIBFUZZER=OFF \
+        -DCMAKE_INSTALL_PREFIX=${TOPLEV}/$stage1_dir/install
+    ninja install
+fi
+
+# Build stage2 clang with instrumentation
+cd $path
+
+echo "Enter name for stage2 profile generation build (default: stage2-prof-gen)"
+read stage2_dir
+
+if [ -z "$stage2_dir" ]; then 
+    stage2_dir="stage2-prof-gen"
+fi
+
+if [ -d "${TOPLEV}/$stage2_dir" ]; then
+    read -p "The '${stage2_dir}' directory already exists. Do you want to continue? (y/n) " answer
+    if [ "$answer" != "y" ]; then
+        echo "Directory ${stage2_dir} already exists, exiting script."
+        exit 1
+    fi
+else
+    mkdir "${TOPLEV}/$stage2_dir"
+    cd "${TOPLEV}/$stage2_dir"
+    
+    CPATH=${TOPLEV}/$stage1_dir/install/bin/    
+    cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD=X86 \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER=$CPATH/clang -DCMAKE_CXX_COMPILER=$CPATH/clang++ \
+        -DLLVM_ENABLE_PROJECTS="clang;lld" \
+        -DLLVM_USE_LINKER=lld -DLLVM_BUILD_INSTRUMENTED=ON \
+        -DCMAKE_INSTALL_PREFIX=${TOPLEV}/$stage2_dir/install
+
+    ninja install
+    
+fi
+
+# Generating profile for PGO
+echo "Enter name for stage3 profile generation build (default: stage3-train)"
+read stage3_train
+
+if [ -z "$stage3_train" ]; then 
+    stage3_train="stage3-train"
+fi
+
+if [ -d "${TOPLEV}/$stage3_train" ]; then
+    read -p "The '${stage3_train}' directory already exists. Do you want to continue? (y/n) " answer
+    if [ "$answer" != "y" ]; then
+        echo "Directory ${stage3_train} already exists, exiting script."
+        exit 1
+    fi
+else
+    mkdir "${TOPLEV}/$stage3_train"
+    cd "${TOPLEV}/$stage3_train"
+    
+    CPATH=${TOPLEV}/$stage2_dir/install/bin    
+    cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD=X86 \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER=$CPATH/clang -DCMAKE_CXX_COMPILER=$CPATH/clang++ \
+        -DLLVM_ENABLE_PROJECTS="clang;lld" \
+        -DLLVM_USE_LINKER=lld -DLLVM_BUILD_INSTRUMENTED=ON \
+        -DCMAKE_INSTALL_PREFIX=${TOPLEV}/$stage3_train/install
+
+    ninja clang   
+fi
+
+cd ${TOPLEV}/stage2-prof-gen/profiles
+${TOPLEV}/stage1/install/bin/llvm-profdata merge -output=clang.profdata *
+
+# Building clang with PGO and LTO
+echo "Enter name for stage4 profile generation build (default: stage2-prof-use-lto)"
+read stage4
+
+if [ -z "$stage4" ]; then 
+    stage4="stage4"
+fi
+
+if [ -d "${TOPLEV}/$stage4" ]; then
+    read -p "The '${stage4}' directory already exists. Do you want to continue? (y/n) " answer
+    if [ "$answer" != "y" ]; then
+        echo "Directory ${stage4} already exists, exiting script."
+        exit 1
+    fi
+else
+    mkdir "${TOPLEV}/$stage4"
+    cd "${TOPLEV}/$stage4"
+    
+    CPATH=${TOPLEV}/$stage1_dir/install/bin/
+    export LDFLAGS="-Wl,-q"
+    cmake -G Ninja ${TOPLEV}/llvm-project/llvm -DLLVM_TARGETS_TO_BUILD=X86 \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER=$CPATH/clang -DCMAKE_CXX_COMPILER=$CPATH/clang++ \
+        -DLLVM_ENABLE_PROJECTS="clang;lld" \
+        -DLLVM_ENABLE_LTO=Full \
+        -DLLVM_PROFDATA_FILE=${TOPLEV}/$stage3_train/profiles/clang.profdata \
+        -DLLVM_USE_LINKER=lld \
+        -DCMAKE_INSTALL_PREFIX=${TOPLEV}/$stage4/install
+    ninja install
+fi
+
+
